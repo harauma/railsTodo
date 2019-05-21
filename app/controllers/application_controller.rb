b9b93d857600d5c1fe4a7a8868ef16b8beb6cc08
unknown <s20164047@stdlan.hcs.local>
2019-05-21 18:06:58 +0900

    アラートとログイン後のパス変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
-        admin_root_path
+        "/todos"
     end
 
     def after_sign_out_path_for(resource)
