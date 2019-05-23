052dc30255896e6ecc67cec98f0245f67df8c544
unknown <s20164047@stdlan.hcs.local>
2019-05-23 16:29:33 +0900

    アカウントの新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
         admin_root_path
     end
 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+      end
 end
