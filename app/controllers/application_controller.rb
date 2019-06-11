6e02325352c58b5e82e7a66d6ba1632c4a6c36d7
原涼馬 <harauma@hararyomanoAir.mshome.net>
2019-06-11 12:04:23 +0900

    新規登録UI変更前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_out_path_for(resource)
-        admin_root_path
+        "/users/login"
     end
 
     before_action :configure_permitted_parameters, if: :devise_controller?
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,7 +29,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to @todo, notice: 'Todo作ったよ！' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.html { redirect_to @todo, notice: 'Todo更新したよ！' }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class TodosController < ApplicationController
   def destroy
     @todo.destroy
     respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
+      format.html { redirect_to todos_url, notice: 'Todo消しちゃった！' }
       format.json { head :no_content }
     end
   end
